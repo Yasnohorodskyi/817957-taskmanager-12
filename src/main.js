fa76ffacedccd0ac785e11dab177fef2beae88fe
Yasnohorodskyi <62336521+Yasnohorodskyi@users.noreply.github.com>
2020-08-08 16:13:05 +0300

    import {createLoadMoreButtonTemplate} from "./view/load-more-button.js";

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-
-const TASK_COUNT = 3;
-
 import {createSiteMenuTemplate} from "./view/site-menu.js";
 import {createFilterTemplate} from "./view/filter.js";
 import {createBoardTemplate} from "./view/board.js";
@@ -8,6 +5,8 @@ import {createSorterTemplate} from "./view/sorter.js";
 import {createTaskTemplate} from "./view/task.js";
 import {createTaskEditTemplate} from "./view/task-edit.js";
 import {createLoadMoreButtonTemplate} from "./view/load-more-button.js";
+
+const TASK_COUNT = 3;
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
